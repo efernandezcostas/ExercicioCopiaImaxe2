34ffa2fd78a6f659280181ba5849201fdfc762ea
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-09-23 08:49:53 +0200

    acabado exercicio 2

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Operaciones op = new Operaciones();
+        op.leerArchivo("/home/dam/Work/Clase/AccesoDatos/foto.jpeg");
+        op.escribirArchivo("/home/dam/Work/Clase/AccesoDatos/foto2.jpeg");
+    }
+}
+
+class Operaciones {
+    ArrayList<Integer> listaBytes = new ArrayList<Integer>();
+
+    public void leerArchivo (String dirName) {
+
+        int lbyte = 0;
+
+        try {
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(dirName));
+                do {
+                    lbyte = bis.read();
+                    if (lbyte != -1) {
+                        listaBytes.add(lbyte);
+                    }
+                    //bis.close();
+                } while (lbyte != -1);
+
+        } catch (IOException e) {
+            System.out.println("No se encontró el archivo");
+        }
+    }
+
+    public void escribirArchivo (String dirName) {
+
+        try {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(dirName));
+            for (Integer listaByte : listaBytes) {
+                bos.write(listaByte);
+            }
+            //bos.close();
+        } catch (IOException e) {
+            System.out.println("No se encontró el archivo");
+        }
+
+    }
+}
